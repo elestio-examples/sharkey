6abeb7359f53545b545a59aa56c8fd842bc159c4
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-07-17 09:07:26 +0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# sharkey
-Deploy Sharkey with CI/CD on Elestio
+# Sharkey CI/CD pipeline
+
+<a href="https://dash.elest.io/deploy?source=cicd&social=dockerCompose&url=https://github.com/elestio-examples/sharkey"><img src="deploy-on-elestio.png" alt="Deploy on Elest.io" width="180px" /></a>
+
+Deploy Sharkey server with CI/CD on Elestio
+
+<img src="sharkey.png" style='width: 100%;'/>
+<br/>
+<br/>
+
+# Once deployed ...
+
+You can open Sharkey here:
+
+    URL: https://[CI_CD_DOMAIN]
+    login: "admin"
+    password: "[ADMIN_PASSWORD]"
+
+You can open pgAdmin here:
+
+    URL: "https://[CI_CD_DOMAIN]:62379"
+    login: "[ADMIN_EMAIL]"
+    password: "[ADMIN_PASSWORD]"
+
+# Custom domain instructions (IMPORTANT)
+
+By default we setup a CNAME on elestio.app domain, but probably you will want to have your own domain.
+
+**_Step1:_** add your domain in Elestio dashboard as explained here:
+
+    https://docs.elest.io/books/security/page/custom-domain-and-automated-encryption-ssltls
+
+**_Step2:_** update the env vars and default.yml to indicate your custom domain
+Open Elestio dashboard > Service overview > click on UPDATE CONFIG button > Env tab
+there update `DOMAIN` with your real domain
+
+**_Step3:_** you must reset the Sharkey instance, you can do that with those commands, connect over SSH and run this:
+
+    cd /opt/app;
+    docker-compose down;
+    rm -rf ./storage;
+    ./scripts/preInstall.sh
+    docker-compose up -d
+    ./scripts/postInstall.sh
+
+You will start over with a fresh instance of Sharkey directly configured with the correct custom domain name and federation will work as expected
---
+++ scripts/postInstall.sh
@@ -0,0 +1,65 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#wait until the server is ready
+echo "Waiting for software to be ready ..."
+sleep 60s;
+
+target=$(docker-compose port web 3000)
+
+# Create account
+curl http://${target}/api/admin/accounts/create \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
+  --data-raw '{"username":"admin","password":"'${ADMIN_PASSWORD}'"}'
+
+# Login
+
+login=$(curl http://${target}/api/signin \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
+  --data-raw '{"username":"admin","password":"'${ADMIN_PASSWORD}'"}')
+
+  access_token=$(echo $login | jq -r '.i' )
+
+# Change smtp
+
+curl http://${target}/api/admin/update-meta \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
+  --data-raw '{"enableEmail":true,"email":"'${FROM_EMAIL}'","smtpSecure":false,"smtpHost":"172.17.0.1","smtpPort":25,"smtpUser":null,"smtpPass":null,"i":"'${access_token}'"}'
+
+
+# Add email
+
+curl http://${target}/api/i/update-email \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
+  --data-raw '{"password":"'${ADMIN_PASSWORD}'","token":null,"email":"'${ADMIN_EMAIL}'","i":"'${access_token}'"}'
+
+# Configure admin
+
+curl http://${target}/api/admin/update-meta \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
+  --data-raw '{"name":"My instance","shortName":null,"description":null,"maintainerName":"admin","maintainerEmail":"'${ADMIN_EMAIL}'","repositoryUrl":"https://github.com/misskey-dev/misskey","impressumUrl":null,"pinnedUsers":[""],"cacheRemoteFiles":false,"cacheRemoteSensitiveFiles":true,"enableServiceWorker":false,"swPublicKey":null,"swPrivateKey":null,"enableFanoutTimeline":true,"enableFanoutTimelineDbFallback":true,"perLocalUserUserTimelineCacheMax":300,"perRemoteUserUserTimelineCacheMax":100,"perUserHomeTimelineCacheMax":300,"perUserListTimelineCacheMax":300,"notesPerOneAd":0,"i":"'${access_token}'"}'
